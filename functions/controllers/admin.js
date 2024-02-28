49428fe1b876579d5702d87aa9e2469f6d78b199
ChoiGeon <qrwrqrwrer@naver.com>
2024-02-28 14:22:37 +0900

    already2

--- functions/controllers/admin.js
+++ functions/controllers/admin.js
@@ -4,75 +4,115 @@ const db = require('firebase-admin').firestore();
 
 // 페이지 //
 exports.adminmatching = async (req,res) => {
-    return res.render('adminmatching',{ csrfToken: req.csrfToken()})
+    try{
+        return res.render('adminmatching',{ csrfToken: req.csrfToken()})   
+    }catch(err){
+        console.log(err);
+        return res.redirect("/error")
+    }
 }
 // 알고리즘 //
 exports.algorithm = async(req,res) => {
-    const {manUid,womanUid} = req.body;
-    // 남자 데이터 불러오기 
-    const manData = (await db.collection('matching_submit').doc(manUid).get()).data()
-    const womanData = (await db.collection('matching_submit').doc(womanUid).get()).data()
-    await db.collection("matching_result").doc(manUid).set({
-        data:womanData,
-        status:"매칭중",
-        otherUid:womanUid,
-    });
-    await db.collection("matching_result").doc(womanUid).set({
-        data:manData,
-        status:"매칭중",
-        otherUid:manUid,
-    });
-    return res.redirect("/gongting-v3/us-central1/api/admin/matching");
+    try{
+        const {manUid,womanUid} = req.body;
+        // 남자 데이터 불러오기 
+        const manData = (await db.collection('matching_submit').doc(manUid).get()).data()
+        const womanData = (await db.collection('matching_submit').doc(womanUid).get()).data()
+        await db.collection("matching_result").doc(manUid).set({
+            data:womanData,
+            status:"매칭중",
+            otherUid:womanUid,
+        });
+        await db.collection("matching_result").doc(womanUid).set({
+            data:manData,
+            status:"매칭중",
+            otherUid:manUid,
+        });
+        return res.redirect("/gongting-v3/us-central1/api/admin/matching");
+    }catch(err){
+        console.log(err);
+        return res.redirect("/error")
+    }
 }
 
 // 학생증 인증 부분 //
 
 // 페이지 //
 exports.checkverify = async(req,res) => {
-    const verifyData =  (await db.collection('users').get())
-    return res.render('adminverifycheck',{verifyData,csrfToken: req.csrfToken()})
+    try{
+        const verifyData =  (await db.collection('users').get())
+        return res.render('adminverifycheck',{verifyData,csrfToken: req.csrfToken()})  
+    }catch(err){
+        console.log(err)
+        return res.redirect("/error")
+    }
 }
 
 // 학생증 인증 성공시 //
 exports.verifyallow = async (req,res) => {
-    const uid = req.params.uid;
-    await db.collection("users").doc(uid).update({
-        verify:"인증완료",
-    })
-    return res.redirect("/gongting-v3/us-central1/api/admin/verify");
+    try{
+        const uid = req.params.uid;
+        await db.collection("users").doc(uid).update({
+            verify:"인증완료",
+        })
+        return res.redirect("/gongting-v3/us-central1/api/admin/verify");
+    }catch(err){
+        console.log(err)
+        return res.redirect("/error")
+    }
 }
 // 학생증 인증 실패시 //
 exports.verifyreject = async (req,res) => {
-    const uid = req.params.uid;
-    const {verifymessage} = req.body;
-    await db.collection("users").doc(uid).update({
-        verify:"반려됨",
-        verifymessage,
-    })
-    return res.redirect("/gongting-v3/us-central1/api/admin/kverify");
+    try{
+        const uid = req.params.uid;
+        const {verifymessage} = req.body;
+        await db.collection("users").doc(uid).update({
+            verify:"반려됨",
+            verifymessage,
+        })
+        return res.redirect("/gongting-v3/us-central1/api/admin/kverify");
+    }catch(err){
+        console.log(err)
+        return res.redirect("/error")
+    }
 }
 
 // 페이지 //
 exports.checkkverify = async(req,res) => {
-    const verifyData =  (await db.collection('users').get())
-    return res.render('adminkverifycheck',{verifyData,csrfToken: req.csrfToken()})
+    try{
+        const verifyData =  (await db.collection('users').get())
+        return res.render('adminkverifycheck',{verifyData,csrfToken: req.csrfToken()})
+    }catch(err){
+        console.log(err)
+        return res.redirect("/error")
+    }
 }
 
 // 학생증 인증 성공시 //
 exports.kverifyallow = async (req,res) => {
-    const uid = req.params.uid;
-    await db.collection("users").doc(uid).update({
-        kverify:"인증완료",
-    })
-    return res.redirect("/gongting-v3/us-central1/api/admin/kverify");
+    try{
+        const uid = req.params.uid;
+        await db.collection("users").doc(uid).update({
+            kverify:"인증완료",
+        })
+        return res.redirect("/gongting-v3/us-central1/api/admin/kverify");
+    }catch(err){
+        console.log(err)
+        return res.redirect("/error")
+    }
 }
 // 학생증 인증 실패시 //
 exports.kverifyreject = async (req,res) => {
-    const uid = req.params.uid;
-    const {kverifymessage} = req.body;
-    await db.collection("users").doc(uid).update({
-        kverify:"반려됨",
-        kverifymessage,
-    })
-    return res.redirect("/gongting-v3/us-central1/api/admin/kverify");
+    try{
+        const uid = req.params.uid;
+        const {kverifymessage} = req.body;
+        await db.collection("users").doc(uid).update({
+            kverify:"반려됨",
+            kverifymessage,
+        })
+        return res.redirect("/gongting-v3/us-central1/api/admin/kverify");
+    }catch(err){
+        console.log(err)
+        return res.redirect("/error")
+    }
 }
--- functions/controllers/page.js
+++ functions/controllers/page.js
@@ -2,10 +2,14 @@ const db = require('firebase-admin').firestore();
 
 // 홈 // 
 exports.home = async(req,res) =>{
-    const {verify,kverify} = (await db.collection('users').doc(req.cookies.uid).get()).data();
-    const isSubmit = (await db.collection('matching_submit').doc(req.cookies.uid).get()).data() ?? undefined;
-    const isResult = (await db.collection('matching_result').doc(req.cookies.uid).get()).data() ?? undefined;
-    return res.render("home",{verify,kverify,isResult,isSubmit})
+    try{ 
+        const {verify,kverify} = (await db.collection('users').doc(req.cookies.uid).get()).data();
+        const isSubmit = (await db.collection('matching_submit').doc(req.cookies.uid).get()).data() ?? undefined;
+        const isResult = (await db.collection('matching_result').doc(req.cookies.uid).get()).data() ?? undefined;
+        return res.render("home",{verify,kverify,isResult,isSubmit})
+    }catch(err){
+        return res.redirect("/error")
+    }
 }
 // 그냥 페이지들 //
 exports.welcome = async (req,res) =>{
@@ -23,55 +27,99 @@ exports.intro = (req,res) =>{
 
 // 내 프로필 // 
 exports.myprofile = async (req,res) =>{
-    const userdata = (await db.collection('userprofile').doc(req.cookies.uid).get()).data()
-    return res.render("myprofile",{userdata})
+    try{
+        const userpdata = (await db.collection('userprofile').doc(req.cookies.uid).get()).data()
+        const userdata = (await db.collection('users').doc(req.cookies.uid).get()).data()
+        return res.render("myprofile",{userpdata,userdata})
+    }catch(err){
+        return res.redirect("/error")
+    }
 }
 
 // 인증 페이지 및 인증 // 
 exports.verifypage = async (req,res) => {
-    const {verify,kverify} = (await db.collection('users').doc(req.cookies.uid).get()).data();
-    return res.render('verifypage',{verify,kverify})
+    try{
+        const {verify,kverify} = (await db.collection('users').doc(req.cookies.uid).get()).data();
+        return res.render('verifypage',{verify,kverify})   
+    }catch(err){
+        return res.redirect("/error")
+    }
 }
 exports.verify = async (req,res) =>{
-    const {verify,verifymessage}=(await db.collection('users').doc(req.cookies.uid).get()).data()
-    return res.render("verify",{uid:req.cookies.uid,verify,verifymessage})
+    try{
+        const {verify,verifymessage}=(await db.collection('users').doc(req.cookies.uid).get()).data()
+        return res.render("verify",{uid:req.cookies.uid,verify,verifymessage})
+    }catch(err){
+        return res.redirect("/error")
+    }
+   
 }
 exports.kverify = async(req,res) =>{
-    const {kverify,kverifymessage}=(await db.collection('users').doc(req.cookies.uid).get()).data()
-    return res.render("kverify",{uid:req.cookies.uid,kverify,kverifymessage})
+    try{
+        const {kverify,kverifymessage}=(await db.collection('users').doc(req.cookies.uid).get()).data()
+        return res.render("kverify",{uid:req.cookies.uid,kverify,kverifymessage})
+    }catch(err){
+        return res.redirect("/error")
+    }
+
 }
 
 // 프로필 설정 // 
 exports.mypset = (req,res) =>{ // 내프로필 설정
-    return res.render("myprofileset",{ csrfToken: req.csrfToken()})
+    try{
+        return res.render("myprofileset",{ csrfToken: req.csrfToken()})
+    }catch(err){
+        return res.redirect("/error")
+    }
+   
 }
 exports.gpset = async (req,res) =>{ // 그룹매칭 프로필 설정
-    const isSubmit = (await db.collection('matching_submit').doc(req.cookies.uid).get()).data() ?? undefined;
-    return res.render("groupmatching",{ isSubmit,csrfToken: req.csrfToken()})
+    try{
+        const isSubmit = (await db.collection('matching_submit').doc(req.cookies.uid).get()).data() ?? undefined;
+        return res.render("groupmatching",{ isSubmit,csrfToken: req.csrfToken()})   
+    }catch(err){
+        return res.redirect("/error")
+    }
 }
 
 // 과팅 신청 재설정 // 
 exports.gpset1 = async (req,res) =>{ // 그룹매칭 프로필 설정
-    const isSubmit = (await db.collection('matching_submit').doc(req.cookies.uid).get()).data() ?? undefined;
-    let groupmatching_count =isSubmit?.wishcount
-    return res.render("groupmatching1",{ isSubmit,groupmatching_count,csrfToken: req.csrfToken()})
+    try{
+        const isSubmit = (await db.collection('matching_submit').doc(req.cookies.uid).get()).data() ?? undefined;
+        let groupmatching_count =isSubmit?.wishcount
+        return res.render("groupmatching1",{ isSubmit,groupmatching_count,csrfToken: req.csrfToken()})   
+    }catch(err){
+        return res.redirect("/error")
+    }
 }
 exports.gpset2 = async (req,res) =>{ // 그룹매칭 프로필 설정
-    const isSubmit = (await db.collection('matching_submit').doc(req.cookies.uid).get()).data() ?? undefined;
-    return res.render("groupmatching2",{ isSubmit,csrfToken: req.csrfToken()})
+    try{
+        const isSubmit = (await db.collection('matching_submit').doc(req.cookies.uid).get()).data() ?? undefined;
+        return res.render("groupmatching2",{ isSubmit,csrfToken: req.csrfToken()})   
+    }catch(err){
+        return res.redirect("/error")
+    }
 }
 exports.gpset3 = async (req,res) =>{ // 그룹매칭 프로필 설정
-    const isSubmit = (await db.collection('matching_submit').doc(req.cookies.uid).get()).data() ?? undefined;
-    return res.render("groupmatching3",{ isSubmit,csrfToken: req.csrfToken()})
+    try{
+        const isSubmit = (await db.collection('matching_submit').doc(req.cookies.uid).get()).data() ?? undefined;
+        return res.render("groupmatching3",{ isSubmit,csrfToken: req.csrfToken()})   
+    }catch(err){
+        return res.redirect("/error")
+    }
 }
 
 // 매칭 결과 //
 exports.matchingresultgroup = async(req,res) =>{
-    const resultdata = (await db.collection('matching_result').doc(req.cookies.uid).get()).data() ?? undefined ;
-    const {status} = (await db.collection('matching_result').doc(resultdata.otherUid).get()).data() ?? undefined ; // otherUid 는 상대방 uid
-    let matchingResult ='';
-    if(resultdata.status == '수락' && status == '수락') matchingResult = '성공';
-    if(resultdata.status == '거절' || status == '거절') matchingResult = '실패';
-
-    return res.render("matchingresultgroup",{resultdata,status,matchingResult});
+    try{
+        const resultdata = (await db.collection('matching_result').doc(req.cookies.uid).get()).data() ?? undefined ;
+        const {status} = (await db.collection('matching_result').doc(resultdata.otherUid).get()).data() ?? undefined ; // otherUid 는 상대방 uid
+        let matchingResult ='';
+        if(resultdata.status == '수락' && status == '수락') matchingResult = '성공';
+        if(resultdata.status == '거절' || status == '거절') matchingResult = '실패';
+    
+        return res.render("matchingresultgroup",{resultdata,status,matchingResult});   
+    }catch(err){
+        return res.redirect("/error")
+    }
 }
